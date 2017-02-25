ae8d7400f43635eace942bcf096d566b6a579d64
defndaines <defndaines@gmail.com>
2017-02-25 09:45:17 -0800

    Adds façades for useful Twitter API calls

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/defndaines/ruiwa"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [twitter-api "0.8.0"]])
---
+++ src/ruiwa/twitter.clj
@@ -0,0 +1,51 @@
+(ns ruiwa.twitter
+  (:require [twitter.oauth :as oauth]
+            [twitter.api.restful :as api]))
+
+;; Note, the Twitter API supports cursors to page through data. At this time,
+;; These functions are assuming we only want a single batch (the latest) in
+;; order to evaluate "friends".
+
+;; Relevant on all responses:
+;; [:headers :x-rate-limit-remaining]
+;; [:status :code]
+
+;; [:body :description]
+;; [:body :profile_image_url_https]
+;; [:body :screen_name]
+;; [:body :url]
+;; [:body :statuses_count]
+;; [:body :favourites_count]
+(defn user-info
+  "Returns a variety of information about the user. The author’s most recent Tweet
+  will be returned inline when possible."
+  [creds user]
+  (api/users-show :oauth-creds creds
+                  :params {:screen-name user :include-entities true}))
+
+;; :body -> [{}]
+;; :in_reply_to_status_id_str
+;; [:entities :hashtags]
+;; [:entities :user_mentions [:screen_name]]
+;; [:user :screen_name]
+(defn user-favorites
+  "Returns the most recent Tweets favorited by the specified user."
+  [creds user]
+  (api/favorites-list :oauth-creds creds
+                      :params {:screen-name user :count 200}))
+
+;; [:entities :hashtags]
+;; [:entities :user_mentions [:screen_name]]
+;; [:retweeted_status :in_reply_to_screen_name] ... when it's a retweet
+(defn user-timeline
+  "Returns a collection of the most recent Tweets posted by the user."
+  [creds user]
+  (api/statuses-user-timeline :oauth-creds creds
+                              :params {:screen-name user :include-rts true :count 200}))
+
+;; [:body :ids []]
+(defn user-friends 
+  "Returns a cursored collection of user IDs for every user the specified user is following."
+  [creds user]
+  (api/friends-ids :oauth-creds creds
+                   :params {:screen-name user :count 5000}))
