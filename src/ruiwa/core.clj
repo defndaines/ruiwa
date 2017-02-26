8f45496924e0e819c8e5125532b96ea9db043a02
defndaines <defndaines@gmail.com>
2017-02-25 17:25:44 -0800

    Scores on five factors

--- src/ruiwa/core.clj
+++ src/ruiwa/core.clj
@@ -1,47 +1,45 @@
 (ns ruiwa.core
   (:require [ruiwa.twitter :as twit]))
 
+;; Values added for each occurrence.
+(def following-val 5)
+(def follower-val 2)
+(def favorited-val 4)
+(def retweet-val 3)
+(def mention-val 2)
+
 (defn following-scores
   "Map users followed with 5 points apiece."
   [creds user]
   (let [{:keys [status body]} (twit/following creds user)]
     (if (= 200 (:code status))
-      (zipmap (:ids body) (repeat 5))
-      {})))
+      (zipmap (:ids body) (repeat following-val)))))
 
 (defn follower-scores
   "Map users followed with 3 points apiece."
   [creds user]
   (let [{:keys [status body]} (twit/followers creds user)]
     (if (= 200 (:code status))
-      (zipmap (:ids body) (repeat 3))
-      {})))
-
-(defn follow-scores
-  "Initialize a map of known user IDs following or followed by the user.
-  Following is worth 5 points, being followed is worth 3."
-  [creds user]
-  (merge-with +
-              (following-scores creds user)
-              (follower-scores creds user)))
+      (zipmap (:ids body) (repeat follower-val)))))
 
 (defn credit-ids
-  "Add 2 points to each user."
-  [scores ids]
-  (reduce (fn [acc e] (update acc e (fnil + 0) 2)) scores ids))
+  "Add points to each user ID."
+  [ids points]
+  (reduce (fn [acc e] (update acc e (fnil + 0) points)) {} ids))
 
 (defn favorited
-  "Gets a list of user IDs where the provided user has favorited their tweets."
+  "Gets scores for user IDs where the provided user has favorited their tweets."
   [creds user]
   (let [{:keys [status body]} (twit/user-favorites creds user)]
     (if (= 200 (:code status))
-      (map #(get-in % [:user :id]) body)
-      [])))
+      (-> (map #(get-in % [:user :id]) body)
+          (credit-ids favorited-val)))))
 
 (defn retweeted-users
   "Extract retweeted user IDs from a timeline body."
   [body]
-  (map #(get-in % [:retweeted_status :user :id]) body))
+  (remove nil?
+          (map #(get-in % [:retweeted_status :user :id]) body)))
 
 (defn user-mentions
   "Extract mentioned user IDs from a timeline body."
@@ -49,3 +47,24 @@
   (->> body
        (map #(get-in % [:entities :user_mentions]))
        (mapcat #(map :id %))))
+
+(defn timelined
+  "Extract user IDs of value from timeline."
+  [creds user]
+  (let [{:keys [status body]} (twit/user-timeline creds user)]
+    (if (= 200 (:code status))
+      (merge-with +
+                  (credit-ids (retweeted-users body) retweet-val)
+                  (credit-ids (user-mentions body) mention-val)))))
+
+(defn score-friends
+  "Assemble scores for user."
+  [creds user]
+  (merge-with +
+              (following-scores creds user)
+              (follower-scores creds user)
+              (favorited creds user)
+              (timelined creds user)))
+
+;; Get the top five:
+;; (take 5 (sort-by val > score-map))
